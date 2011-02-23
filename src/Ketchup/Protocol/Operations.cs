b35c1f8b70f34eb9808d40b0d199d3765a36e6ab
unknown <jsirota@.intranet.theknot.com>
2011-02-22 16:51:12 -0800

    removed deleted files from git

--- src/Ketchup/Protocol/Operations.cs
+++ src/Ketchup/Protocol/Operations.cs
@@ -15,23 +15,18 @@ namespace Ketchup.Protocol.Asynchronous {
 			Action miss,
 			Action<Exception> error
 			) {
-			try {
-				key = config.GetKey(key, bucket);
-				var packet = new Packet<T>().Operation(operation).Key(key);
-
-				var node =  Hashing.GetNode(key, bucket).Request(packet.Serialize(),
-					rb => { hit(packet.Deserialize(rb)); },
-					ex => {
-						if (ex is Protocol.NotFoundException) {
-							miss();
-						} else {
-							error(ex);
-						}
+			key = config.GetKey(key, bucket);
+			var packet = new Packet<T>().Operation(operation).Key(key);
+			var node = Hashing.GetNode(key, bucket).Request(packet.Serialize(),
+				rb => { hit(packet.Deserialize(rb)); },
+				ex => {
+					if (ex is Protocol.NotFoundException) {
+						miss();
+					} else {
+						error(ex);
 					}
-				);
-			} catch (Exception ex) {
-				error(ex);
-			}
+				}
+			);
 		}
 	}
 }
