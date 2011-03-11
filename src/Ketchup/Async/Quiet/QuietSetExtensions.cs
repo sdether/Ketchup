7484b5ebee756ef0cbf02b4ec29a4fd437648d39
jasonsirota <jason@sirotamail.com>
2011-03-10 23:45:12 -0800

    split quiet and silent functions into subnamespaces

---
+++ src/Ketchup/Async/Quiet/QuietSetExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Ketchup.Async;
+
+namespace Ketchup.Async.Quiet {
+
+	public static class QuietSetExtensions {
+
+		public static KetchupClient Set<T>(this KetchupClient client, string key, T value, Action<Exception> error) {
+			return client.Set(key, value, null, error);
+		}
+
+		public static KetchupClient Set<T>(this KetchupClient client, string key, T value, TimeSpan expiration, Action<Exception> error) {
+			return client.Set(key, value, expiration, null, error);
+		}
+
+		public static KetchupClient Set<T>(this KetchupClient client, string key, T value, DateTime expiration, Action<Exception> error) {
+			return client.Set(key, value, expiration, null, error);
+		}
+	}
+}
+
---
+++ src/Ketchup/Async/Silent/SilentSetExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Ketchup.Async;
+
+namespace Ketchup.Async.Silent {
+
+	public static class SilentSetExtensions {
+
+		public static KetchupClient Set<T>(this KetchupClient client, string key, T value) {
+			return client.Set(key, value, null, null);
+		}
+
+		public static KetchupClient Set<T>(this KetchupClient client, string key, T value, TimeSpan expiration) {
+			return client.Set(key, value, expiration, null, null);
+		}
+
+		public static KetchupClient Set<T>(this KetchupClient client, string key, T value, DateTime expiration) {
+			return client.Set(key, value, expiration, null, null);
+		}
+	}
+}
+
