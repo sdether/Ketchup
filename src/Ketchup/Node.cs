9896c2e5d646dd103d07140f25589123fd1d2ba2
jasonsirota <jason@sirotamail.com>
2011-03-16 23:41:51 -0700

    cleaned up Node a bit

--- src/Ketchup/Node.cs
+++ src/Ketchup/Node.cs
@@ -22,7 +22,7 @@ namespace Ketchup {
 
 		public Node() {
 			IsDead = false;
-			CurrentRetryCount = 0;
+			CurrentRetryCount = -1;
 			DeadAt = DateTime.MinValue;
 			LastConnectionFailure = DateTime.MinValue;
 		}
@@ -50,8 +50,7 @@ namespace Ketchup {
 			var remote = state.Socket;
 
 			remote.EndReceive(asyncResult);
-			if (state.Callback != null)
-				state.Callback(_buffer);
+			if (state.Callback != null) state.Callback(_buffer);
 		}
 
 		private Socket Connect() {
@@ -59,38 +58,24 @@ namespace Ketchup {
 		}
 
 		private Socket Connect(Socket socket) {
-			if (IsDead) 
-				throw new Exception("Node is dead");
-
-			if (socket != null && socket.Connected)
-				return socket;
-
-			if (string.IsNullOrEmpty(Host))
-				throw new Exception("Host is null or empty string, cannot connect to socket");
-
-			if (Port == default(int))
-				throw new Exception("Port is not valid for this Node, cannot connect to socket");
-
-			if (socket == null)
-				socket = CreateSocket();
+			if (IsDead) throw new Exception("Node is dead");
+			if (socket != null && socket.Connected) return socket;
+			if (string.IsNullOrEmpty(Host)) throw new Exception("Host is null or empty string, cannot connect to socket");
+			if (Port == default(int)) throw new Exception("Port is not valid for this Node, cannot connect to socket");
+			if (socket == null) socket = CreateSocket();
 
 			try {
 				//if a connection error happened before, recurse until reconnect time has passed
-				if(!ReadyToTry()) 
-					return Connect(socket);
+				if(!ReadyToTry()) return Connect(socket);
 
 				//the big kahuna
 				socket.Connect(Host, Port);
 
 			} catch (SocketException ex) {
-
 				//at least retry timeouts the specified number of times
-				if(ex.SocketErrorCode == SocketError.TimedOut) 
-					return HandleTimeout(socket);
-				
+				if(ex.SocketErrorCode == SocketError.TimedOut) return HandleTimeout(socket);
 				throw;
 			}
-
 			return socket;
 		}
 
@@ -107,34 +92,27 @@ namespace Ketchup {
 		}
 
 		private bool ReadyToTry() {
-			if(LastConnectionFailure == DateTime.MinValue)
-				return true;
-
+			if(LastConnectionFailure == DateTime.MinValue) return true;
 			return (DateTime.Now - LastConnectionFailure) >= KetchupConfig.Current.ConnectionRetryDelay;
 		}
 
 		private Socket HandleTimeout(Socket socket) {
 			LastConnectionFailure = DateTime.Now;
 			
-			//haven't reached the max retries yet, let's try again until we do
-			if (CurrentRetryCount < KetchupConfig.Current.ConnectionRetryCount){
-				CurrentRetryCount++;
-				return Connect(socket);
+			if (CurrentRetryCount++ >= KetchupConfig.Current.ConnectionRetryCount) {
+				IsDead = true;
+				DeadAt = LastConnectionFailure;
+				return socket;
 			}
 
-			IsDead = true;
-			DeadAt = LastConnectionFailure;
-			
-			return socket;
+			//haven't reached the max retries yet, let's try again until we do
+			return Connect(socket);
 		}
 
 		public static int GetPort(string endpoint) {
 			int port;
-
 			var portString = endpoint.Contains(":") ? endpoint.Split(':')[1] : "11211";
-			if (!int.TryParse(portString, out port))
-				throw new ConfigurationErrorsException("The specified port is not a valid int integer");
-
+			if (!int.TryParse(portString, out port)) throw new ConfigurationErrorsException("The specified port is not a valid int integer");
 			return port;
 		}
 
