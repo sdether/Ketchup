92d87f098e4cae5a0aa6b38fe056cac985e9e68f
jasonsirota <jason@sirotamail.com>
2011-03-16 23:35:31 -0700

    cleaned up Node a bit

--- src/Ketchup/Node.cs
+++ src/Ketchup/Node.cs
@@ -6,15 +6,15 @@ using Ketchup.Config;
 namespace Ketchup {
 	public class Node {
 
-		private readonly byte[] buffer = new byte[1024];
+		private readonly byte[] _buffer = new byte[1024];
 		private Socket _socket;
 		
-		public int		Port					{ get; set; }
-		public int		CurrentRetryCount		{ get; set; }
-		public bool		IsDead					{ get; set; }
-		public string	Host					{ get; set; }
-		public DateTime DeadAt					{ get; set; }
-		public DateTime LastConnectionFailure	{ get; set; }
+		public int Port { get; set; }
+		public int CurrentRetryCount { get; set; }
+		public bool	IsDead { get; set; }
+		public string Host { get; set; }
+		public DateTime DeadAt { get; set; }
+		public DateTime LastConnectionFailure { get; set; }
 
 		public string Id {
 			get { return Host + ":" + Port; }
@@ -32,58 +32,47 @@ namespace Ketchup {
 			Port = port;
 		}
 
-		public Node Request(byte[] packet, Action<byte[]> callback) {
-			if (IsDead)
-				throw new Exception("Node is dead");
-
-			//TODO: Make connect async? not sure if there is a benefit to blocking an async connect for race conditions
-			var socket = Connect(_socket);
-			var state = new NodeAsyncState(){ Callback = callback, Socket = socket};
-			socket.BeginSend(packet, 0, packet.Length, SocketFlags.None, new AsyncCallback(SendData), state);
-
-			return this;
+		public void Request(byte[] packet, Action<byte[]> callback) {
+			var state = new NodeAsyncState(){ Callback = callback, Socket = Connect()};
+			state.Socket.BeginSend(packet, 0, packet.Length, SocketFlags.None, SendData, state);
 		}
 
 		private void SendData(IAsyncResult asyncResult) {
 			var state = (NodeAsyncState)asyncResult.AsyncState;
 			var remote = state.Socket;
+
 			remote.EndSend(asyncResult);
-			remote.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveData), state);			
+			remote.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, ReceiveData, state);			
 		}
 
 		private void ReceiveData(IAsyncResult asyncResult) {
 			var state = (NodeAsyncState)asyncResult.AsyncState;
 			var remote = state.Socket;
-			remote.EndReceive(asyncResult);
 
+			remote.EndReceive(asyncResult);
 			if (state.Callback != null)
-				state.Callback(buffer);
+				state.Callback(_buffer);
 		}
 
-		private static Socket CreateSocket() {
-			var config = KetchupConfig.Current;
-			
-			return new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp) {
-				SendTimeout = config.ConnectionTimeout.Milliseconds,
-				ReceiveTimeout = config.ConnectionTimeout.Milliseconds,
-				NoDelay = true,
-				Blocking = true,
-				UseOnlyOverlappedIO = false
-			};
+		private Socket Connect() {
+			return Connect(_socket);
 		}
 
 		private Socket Connect(Socket socket) {
+			if (IsDead) 
+				throw new Exception("Node is dead");
+
 			if (socket != null && socket.Connected)
 				return socket;
-
-			if (socket == null)
-				socket = CreateSocket();
 
 			if (string.IsNullOrEmpty(Host))
 				throw new Exception("Host is null or empty string, cannot connect to socket");
 
 			if (Port == default(int))
 				throw new Exception("Port is not valid for this Node, cannot connect to socket");
+
+			if (socket == null)
+				socket = CreateSocket();
 
 			try {
 				//if a connection error happened before, recurse until reconnect time has passed
@@ -103,6 +92,18 @@ namespace Ketchup {
 			}
 
 			return socket;
+		}
+
+		private static Socket CreateSocket() {
+			var config = KetchupConfig.Current;
+
+			return new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp) {
+				SendTimeout = config.ConnectionTimeout.Milliseconds,
+				ReceiveTimeout = config.ConnectionTimeout.Milliseconds,
+				NoDelay = true,
+				Blocking = true,
+				UseOnlyOverlappedIO = false
+			};
 		}
 
 		private bool ReadyToTry() {
