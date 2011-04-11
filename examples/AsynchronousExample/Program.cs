c59c404a2e2e628988f29dbd947cfe5efd1bb0ee
jasonsirota <jason@sirotamail.com>
2011-04-11 15:40:14 -0700

    cleaned up async example

--- examples/AsynchronousExample/Program.cs
+++ examples/AsynchronousExample/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using Ketchup;
 using Ketchup.Async;
-using Ketchup.Config;
 
 public class Program
 {
@@ -9,57 +8,65 @@ public class Program
 	private static readonly string _key = "key-async";
 	private static readonly string _value = "key-async-value";
 
-	static void Main(string[] args)
+	public static void Main(string[] args)
 	{
 		//Initialize Ketchup Client
-		var config = new KetchupConfig("default", "172.17.6.201", 11211);
+		var config = new Ketchup.Config.KetchupConfig("default", "127.0.0.1", 11211);
 		var client = new KetchupClient(config);
 		_bucket = client.GetBucket("default");
 
 		//Set asyncrhonously, call OnSetSuccess() on success and OnSetError on Exception
 		_bucket.Set(_key, _value, OnSetSuccess<string>, OnSetError, null);
-		Console.ReadLine();
 	}
 
-	public static void OnSetError(Exception ex, object state)
+	private static void OnSetSuccess<T>(object state)
 	{
-		Console.WriteLine("Set command for key '" + _key + "' failed with error " + ex.Message);
-		Console.WriteLine("Pess any key to continue...");
-	}
-
-	public static void OnSetSuccess<T>(object state)
-	{
-		//set was successful, try Get
+		//Set was successful, now execute Get async
+		//Pass callbacks for OnGetHit, OnGetMiss, OnGetError
 		_bucket.Get<T>(_key, OnGetHit, OnGetMiss, OnGetError, null);
 	}
 
-	public static void OnGetMiss(object state)
+	private static void OnGetHit<T>(T value, object state)
 	{
-		Console.WriteLine("Get command for key '" + _key + "' returned miss");
-		Console.WriteLine("Pess any key to continue...");
-	}
-
-	public static void OnGetError(Exception ex, object state)
-	{
-		Console.WriteLine("Get command for key '" + _key + "' failed with exception '" + ex.Message + "'");
-		Console.WriteLine("Pess any key to continue...");
-	}
-
-	public static void OnGetHit<T>(T value, object state)
-	{
-		//Hit was successful, now delete it
+		//OnGetHit success, now execute Delete
+		//Pass callbacks for OnDeleteSuccess and OnDeleteError
 		_bucket.Delete(_key, OnDeleteSuccess, OnDeleteError, null);
 	}
 
-	public static void OnDeleteError(Exception ex, object state)
+	private static void OnDeleteSuccess(object state)
 	{
-		Console.WriteLine("Delete command for key '" + _key + "' failed with exception '" + ex.Message + "'");
-		Console.WriteLine("Pess any key to continue...");
+		//Asynchronous path through Set, Get, Delete was successul, exit program
+		Console.WriteLine("Set, Get and Delete commands for key '" + _key + "' were successful");
+		Finish();
 	}
 
-	public static void OnDeleteSuccess(object state)
+	private static void Finish()
 	{
-		Console.WriteLine("Set, Get and Delete commands for key '" + _key + "' were successful");
 		Console.WriteLine("Pess any key to continue...");
+		Console.ReadLine();
+	}
+
+	private static void OnGetMiss(object state)
+	{
+		Console.WriteLine("Get command for key '" + _key + "' returned miss");
+		Finish();
+	}
+
+	private static void OnSetError(Exception ex, object state)
+	{
+		Console.WriteLine("Set command for key '" + _key + "' failed with error " + ex.Message);
+		Finish();
+	}
+
+	private static void OnGetError(Exception ex, object state)
+	{
+		Console.WriteLine("Get command for key '" + _key + "' failed with exception '" + ex.Message + "'");
+		Finish();
+	}
+
+	private static void OnDeleteError(Exception ex, object state)
+	{
+		Console.WriteLine("Delete command for key '" + _key + "' failed with exception '" + ex.Message + "'");
+		Finish();
 	}
 }
